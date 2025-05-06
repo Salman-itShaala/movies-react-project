9bacd788d54fbef60c1eb6ad838017fbc9ee0df7
salman-itShaala <salman.shaikh@itshaala.com>
2025-05-06 15:16:02 +0530

    added header

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import "./App.css";
 import Home from "./pages/Home";
 import AllMovies from "./pages/AllMovies";
 import Movie from "./pages/Movie";
+import Header from "./components/Header";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/all-movies" element={<AllMovies />} />
